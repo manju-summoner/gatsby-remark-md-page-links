7d5f2bd005114dd8f53c4b4d0bdb8612c954ee61
manju-summoner <manju_summoner@manjubox.net>
2022-07-14 23:38:29 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# gatsby-remark-md-page-links
+```md
+You can create links to .md page by [relative file path](./README.md) instead of [url style link](../README/)
+```
+
+## How to use
+```js:gatsby-config.js
+plugins: [
+  {
+    resolve: `gatsby-transformer-remark`,
+    options: {
+      plugins: [
+          require.resolve("./plugins/gatsby-remark-md-page-links"),
+      ],
+    },
+  },
+],
+```
+or
+```js:gatsby-config.js
+plugins: [
+  {
+    resolve: `gatsby-plugin-mdx`,
+    options: {
+      gatsbyRemarkPlugins: [
+        require.resolve("./plugins/gatsby-remark-md-page-links"),
+      ],
+    },
+  },
+],
+```
---
+++ index.js
@@ -0,0 +1,25 @@
+const visit = require('unist-util-visit');
+module.exports = ({markdownAST, markdownNode}) => {
+  const isIndex = markdownNode.fileAbsolutePath.endsWith('/index.mdx') || markdownNode.fileAbsolutePath.endsWith('/index.md');
+  visit(markdownAST, 'link', node => {
+    if (
+      node.url &&
+      !node.url.startsWith('//') &&
+      !node.url.startsWith('http') &&
+      !node.url.startsWith('https')
+    ) {
+        const basePath = node.url.startsWith('/') || isIndex ? '' : '../';
+        node.url = node.url.replace(/(.*).mdx?(#.*)?$/, (match, path, hash) => {
+          if(path.endsWith('/index')) 
+            path = path.replace('/index', '');
+            
+          if(hash)
+              return `${basePath}${path}/#${hash}`
+          else
+              return `${basePath}${path}/`
+        })
+    }
+  });
+
+  return markdownAST;
+};
